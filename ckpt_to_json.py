ef5127247103ffecf9324d2076effb1cbbb6e79c
Michael Hannalla <michael.sami.hanna@gmail.com>
2021-11-04 01:26:04 +0200

    Added ckpt to json converter (inplace)

---
+++ ckpt_to_json.py
@@ -0,0 +1,19 @@
+import argparse
+import tensorflow as tf
+
+if __name__ == "__main__":
+
+    # Argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ckpt_path", default= "models/distracted_driver_detector_v1.ckpt")
+    args = parser.parse_args()
+
+    # Load the ckpt model
+    model = tf.keras.models.load_model(args.ckpt_path)
+    # serialize model to JSON
+    model_json = model.to_json()
+    with open(args.ckpt_path + "/../model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    model.save_weights(args.ckpt_path + "/../model.h5")
+    print("Saved model to as json")
