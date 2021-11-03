b70d2611d23af9ce45e9fa5cd82444639dc445f1
Michael Hannalla <michael.sami.hanna@gmail.com>
2021-11-04 01:42:19 +0200

    Made a more generic inference script

--- inference.py
+++ inference.py
@@ -2,12 +2,14 @@ import cv2
 import argparse
 import numpy as np
 import tensorflow as tf 
+import time
+from tensorflow.keras.models import model_from_json
 
 if __name__ == "__main__":
 
     # Argument parser
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_path", default= "models/distracted_driver_detector_v1.ckpt") 
+    parser.add_argument("--model_path", default= "models/distracted_driver_detector_v1") 
     parser.add_argument("--folder", default=None)
     parser.add_argument("--camera", default=None, type= int)
     args = parser.parse_args()
@@ -16,7 +18,14 @@ if __name__ == "__main__":
         "c4 Talking on the phone (left hand)", "c5 Operating the radio", "c6 Drinking", "c7 Reaching behind", "c8 Hair and makeup", 
         "c9 Talking to passenger(s)"]       # List of class names
 
-    model = tf.keras.models.load_model(args.model_path)
+    # load json and create model
+    json_file = open(args.model_path + '.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    # load weights into new model
+    model.load_weights(args.model_path+ ".h5")
+    print("Loaded model from disk")
 
     if args.camera == None and args.folder == None:
         raise RuntimeError("Please specify the input to either being from a folder or a camera")
@@ -24,6 +33,7 @@ if __name__ == "__main__":
     if not args.camera == None:
         cap = cv2.VideoCapture(args.camera)
         while cap.isOpened():
+            tic = time.time()
             ret, frame = cap.read()
             if ret == True:
                 frame_resized = cv2.resize(frame, (128, 128))
@@ -33,7 +43,14 @@ if __name__ == "__main__":
 
                 print("Predicted Driver Status: {}".format(class_names[pred_label]))
                 cv2.imshow("input frame", frame_resized)
-                cv2.waitKey()
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+
+            toc = time.time()
+            print("Inference Time: {}s".format(toc-tic))
+            print("FPS: {}s".format(1/(toc-tic)))
+            
+
     
     elif not args.folder == None:
         raise NotImplementedError("Still under development!")
